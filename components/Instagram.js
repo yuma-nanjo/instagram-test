dac66f98130f40e68f94653b568d0c0df94d0ce7
yuma-nanjo <y.nanjo1110@gmail.com>
2022-08-18 19:31:10 +0900

    ifbun

--- components/Instagram.js
+++ components/Instagram.js
@@ -2,21 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 
 function Instagram({ posts }) {
-  return (
-    <div>
-      {posts.business_discovery.media.data.map((data) => (
-        <Link href={data.permalink} alt={data.id} key={data.id}>
-          <Image
-            src={data.media_url}
-            width={360}
-            height={360}
-            alt="Photo"
-            objectFit="cover"
-          />
-        </Link>
-      ))}
-    </div>
-  );
+  if(posts.business_discovery.media !== undefined){
+    return (
+      <div>
+        {posts.business_discovery.media.data.map((data) => (
+          <Link href={data.permalink} alt={data.id} key={data.id}>
+            <Image
+              src={data.media_url}
+              width={360}
+              height={360}
+              alt="Photo"
+              objectFit="cover"
+            />
+          </Link>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Instagram;
