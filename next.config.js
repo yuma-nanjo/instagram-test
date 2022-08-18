e786b2e24b06be39d05165a548a5e1b07bab91b5
yuma-nanjo <y.nanjo1110@gmail.com>
2022-08-18 19:01:40 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "scontent-sjc3-1.cdninstagram.com"],
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
