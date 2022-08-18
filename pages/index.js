f09a343cfc4977effc031fc0b609e8d248bf5e44
yuma-nanjo <y.nanjo1110@gmail.com>
2022-08-18 19:22:35 +0900

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import Instagram from "./components/Instagram";
+import Instagram from "../components/Instagram";
 
 export default function Home({ posts }) {
   return (
