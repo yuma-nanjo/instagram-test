a9f8739bc6958a2f5c64832a382fafd5f4241006
yuma-nanjo <y.nanjo1110@gmail.com>
2022-08-18 20:17:16 +0900

    commit

--- components/Instagram.js
+++ components/Instagram.js
@@ -9,15 +9,7 @@ function Instagram({ posts }) {
     return (
       <div>
         {posts.business_discovery.media.data.map((data) => (
-          <Link href={data.permalink} alt={data.id} key={data.id}>
-            <Image
-              src={data.media_url}
-              width={360}
-              height={360}
-              alt="Photo"
-              objectFit="cover"
-            />
-          </Link>
+          <img src={data.media_url} alt="photo" key={data.id} />
         ))}
       </div>
     );
